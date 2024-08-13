3d8115e3c30e8de49b9a116039865bc9b6a216bc
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-08-14 02:13:22 +0800

    1.0.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
-import { Square } from "chess.js";
-import { ChessboardOptions, ChessboardProps, Theme } from "./types";
+import { Chess, Square } from "chess.js";
+import { BufferOptions, ChessboardOptions, ChessboardProps, Theme } from "./types";
 declare class Chessboard implements ChessboardProps {
     private cols;
     private chess;
@@ -13,12 +13,23 @@ declare class Chessboard implements ChessboardProps {
     style: Theme;
     flipped: boolean;
     constructor(options?: ChessboardOptions);
+    private get width();
+    private get height();
+    setTheme(theme: Theme): void;
     loadPGN(pgn: string): void;
     loadFEN(fen: string): void;
     highlightSquares(...squares: Square[]): void;
-    buffer(mime?: "image/png" | "image/jpeg" | "image/webp" | "image/avif"): Promise<Buffer>;
-    png(path: string): Promise<void>;
-    setTheme(theme: Theme): void;
+    private drawBoard;
+    /**
+     * Returns a `Chess` instance representing the state of the game up to the specified move.
+     * This method generates a snapshot of the chess game as it was after the given move number in the PGN string.
+     * @param move - The move number in the PGN string up to which the game should be captured.
+     *               For example:
+     *               - If `move` is `2`, it returns the `Chess` instance with the game state reflecting all moves up to and including the second move.
+     *               - If `move` is `4`, it returns the `Chess` instance with the game state reflecting all moves up to and including the fourth move.
+     * @returns A `Chess` instance with the game state up to the provided move.
+     */
+    getSnapshot(move: number): Chess;
+    buffer(mime?: "image/png" | "image/jpeg" | "image/webp" | "image/avif" | "image/gif", options?: BufferOptions): Promise<Buffer>;
 }
-export { Theme };
-export default Chessboard;
+export { Chessboard, Theme };
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -18,6 +18,24 @@ export interface ChessboardOptions {
     /** The desired style of the chess pieces */
     style?: Theme;
 }
+export interface BufferOptions {
+    /** The delay between each frame, 500 by default. */
+    delay?: number;
+    /** Whether to highlight the latest action made. */
+    highlight?: boolean;
+    /**
+     * Specifies the move number in a PGN string to which the game should be played up to.
+     * For example:
+     * - If `move` is `2`, and the PGN string is "1. Nc3 Nf6 2. Ne4", the result should be "1. Nc3 Nf6".
+     * - If `move` is `1`, the result should be "1. Nc3".
+     *
+     * The `move` number represents the move in the sequence of the game, where moves are counted from 1.
+     *
+     * If an `image/gif` is provided, it will display a GIF of the game up to the specified move.
+     * For any other file type, only the immediate move image corresponding to the specified move will be shown.
+     */
+    move?: number;
+}
 export interface ChessboardProps extends ChessboardOptions {
     /** Loads PGN into Chess.js instance */
     loadPGN: (pgn: string) => void;
@@ -27,8 +45,5 @@ export interface ChessboardProps extends ChessboardOptions {
     highlightSquares: (...squares: Square[]) => void;
     /** Set the theme of the chessboard */
     setTheme: (theme: Theme) => void;
-    /** Generates buffer image */
-    buffer: (mime: "image/png" | "image/jpeg" | "image/webp" | "image/avif") => Promise<Buffer>;
-    /** Generates png image */
-    png: (path: string) => Promise<void>;
+    buffer: (mime: "image/png" | "image/jpeg" | "image/webp" | "image/avif" | "image/gif", options?: BufferOptions) => Promise<Buffer>;
 }
